20b21dcc1655600155ef4d6af715af8d79a4635a
Houzifa Habbo <h-hebbo@hotmail.com>
2023-03-25 00:42:16 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(names,string){
+    return names.filter(names => names.toLowerCase() === string.toLowerCase());
+}
+function fuzzyMatch(names,string){
+    return names.filter(names => names.startsWith(string));
+  }
+function matchName(names,string){
+    return names.filter(names => names.name === string);
+}
